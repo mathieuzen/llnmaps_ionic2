0df4b871a4bada528fd05e8bb45036e86183a564
Mathieu Zen <zen.mathieu@gmail.com>
2016-05-12 17:00:56 +0200

    help and info pages

---
+++ app/pages/help/help.js
@@ -0,0 +1,15 @@
+import {Page, NavController, NavParams} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/help/help.html'
+})
+export class HelpPage {
+  static get parameters() {
+    return [];
+  }
+
+  constructor() {
+  }
+
+}
---
+++ app/pages/info/info.js
@@ -0,0 +1,15 @@
+import {Page, NavController, NavParams} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/info/info.html'
+})
+export class InfoPage {
+  static get parameters() {
+    return [];
+  }
+
+  constructor() {
+  }
+
+}
